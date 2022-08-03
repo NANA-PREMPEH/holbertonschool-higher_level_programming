44767abd330fc482829d85b35e286bd0b7758b25
Williams Osei Prempeh <52207181+NANA-PREMPEH@users.noreply.github.com>
2022-08-03 03:45:13 +0000

    Update 12-pascal_triangle.py

--- 0x0B-python-input_output/12-pascal_triangle.py
+++ 0x0B-python-input_output/12-pascal_triangle.py
@@ -1,12 +1,16 @@
 #!/usr/bin/python3
-"""Function Declaration"""
+""" pascal triangle"""
 
 
-def pascal_triangle(n):
-    list1 = []
-    list2 = []
-    if n == 1:
-        return [[1]]
-    for r in range(1, n):
-        if l in r:
-            l = [1]
+def pascal_triangle(n=4500):
+    """print pascal"""
+    pascal = [[0]*i for i in range(1, n+1)]
+    cmpt = 0
+    for i in range(n):
+        pascal[i][0] = 1
+        pascal[i][-1] = 1
+        for j in range(0, i//2):
+            pascal[i][j+1] = pascal[i-1][j] + pascal[i-1][j+1]
+            pascal[i][i-j-1] = pascal[i-1][j] + pascal[i-1][j+1]
+
+    return pascal
